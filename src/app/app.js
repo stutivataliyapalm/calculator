5b096d240e4c2d3dc4ab553d16bc4d62b48a8473
stuti hiteshbhai vataliya <136146687+stutivataliyapalm@users.noreply.github.com>
2025-09-28 11:16:52 +0530

    Delete src directory

--- src/app/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app'; // Ensure the correct path to App component
-
-ReactDOM.render(
-  <App />, // Render only the App component
-  document.getElementById('root')
-);
